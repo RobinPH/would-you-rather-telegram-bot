bae4e9297c351b14bf28f3100cea01c25457b4d3
RobinPH <robindimasin@gmail.com>
2020-06-23 20:59:29 +0800

    Fixed Path

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 (async() => {
   await connectDB(process.env.ATLAS_URI!);
-
+  
   const defaultQuestions = await getDefaultQuestions();
   ratherBot(process.env.TOKEN!, defaultQuestions);
 })();
