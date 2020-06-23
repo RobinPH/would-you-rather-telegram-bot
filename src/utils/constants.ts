3045c8c4e45ee69e1b32c69937812d934c43997c
RobinPH <robindimasin@gmail.com>
2020-06-23 22:30:18 +0800

    Changed SUGGESTION

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,6 +4,6 @@ export enum EXAMPLE {
 }
 
 export const FORMAT = `Format: "Option1 or Option2"`;
-export const SUGGESTION = `*Suggest New Would Your Rather Question.* \n  ${ FORMAT } \n  ${ EXAMPLE.ONE } \n  ${ EXAMPLE.TWO } \n\n_Reply to this Message._`;
+export const SUGGESTION = `*Suggest Would Your Rather Question.* \n  ${ FORMAT } \n  ${ EXAMPLE.ONE } \n  ${ EXAMPLE.TWO } \n\n_Reply to this Message._`;
 export const INVALID = `*Invalid Suggestion Format* \n  ${ FORMAT } \n  ${ EXAMPLE.ONE } \n  ${ EXAMPLE.TWO }`;
 export const ADDED_SUCCESSFUL = `*Added new Would You Rather question.*`;
